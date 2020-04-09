4b54eec619461ca59d7f0e7fbc08800ac54a1550
Artem Pixaykin <artem.pixaykin@yandex.ru>
2020-04-10 01:24:06 +0500

    finito

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Count
 from django.views import View
 from django.shortcuts import render
 from jobs.models import Company, Specialty, Vacancy
